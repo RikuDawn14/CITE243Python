49864904c9588f7eecd53e41ab39527488796db4
RikuDawn14 <180044602+RikuDawn14@users.noreply.github.com>
2025-09-22 09:46:23 -0700

    start of coding

--- assignments/ch09 pattern matching.py
+++ assignments/ch09 pattern matching.py
@@ -1,1 +1,35 @@
 # Chapter 9 (TEXT PATTERN MATCHING WITH REGULAR EXPRESSIONS) Assignment
+
+import re
+
+# Function to check password strength.
+def checker():
+
+    pattern = re.compile(r'''(
+         ^(?=.*[a-z])
+          (?=.*[A-Z])
+          (?=.*\d)
+          (?=.*[@$#%])
+          [A-Za-z\d@$#%]
+          {8,20}$            
+        
+        )''', re.VERBOSE)
+
+
+
+
+# Function to get user input password.
+def password_input():
+    
+    password = input('Please enter a password to check security, then hit ENTER. \n=>')
+
+    if len(password) == 0:
+        print("You didn't enter a password! Try again.")
+
+    else:
+        checker(password)
+
+
+
+
+
