93e8cf242a1870d5bf801521855ae8df57f6ea97
RikuDawn14 <180044602+RikuDawn14@users.noreply.github.com>
2025-09-29 13:27:41 -0700

    f

---
+++ assignments/ch11_make_zip.py
@@ -0,0 +1,9 @@
+# ch11 convert directory into zip.
+
+import os
+import shutil
+import time
+
+# User input on folder location to compress
+# User input on save location for zip
+# Zip folder
--- assignments/ch11_mkae_zip.py
+++
@@ -1,5 +0,0 @@
-# ch11 convert directory into zip.
-
-import os
-import shutil
-import time
