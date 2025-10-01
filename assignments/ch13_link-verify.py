d95588e5e989060143761a9a2ad6a0841a58dc2e
RikuDawn14 <180044602+RikuDawn14@users.noreply.github.com>
2025-10-01 09:31:32 -0700

    Created file and set goals

---
+++ assignments/ch13_link-verify.py
@@ -0,0 +1,22 @@
+# Given the URL of a web page, will find every <a> link.
+"""
+===========================================================
+Program Name: ch13 link verify
+Author: Matthew Balthaser
+Date: 2025-09-28
+Description:
+    This program converts given the URL of a web page, will find every <a> link on the page.
+    It is designed to test whether the linked URL results in a “404 Not Found” status code.
+    
+Usage:
+    Run the script using Python 3.13.7. Ensure all dependencies
+    are installed before execution.
+
+===========================================================
+"""
+
+# Take user input for URL.
+# Verify URL is valid.
+# Find every "a" name on page.
+# Test pages.
+# Return any links that are broken.
