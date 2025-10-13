061413d1d7a5c43167585fb14f22442e631dd4f8
RikuDawn14 <180044602+RikuDawn14@users.noreply.github.com>
2025-10-13 11:22:52 -0700

    updates

--- assignments/ch13_Image-Downloader.py
+++ assignments/ch13_Image-Downloader.py
@@ -24,7 +24,7 @@ import textwrap
 def user_input():
     
     while True:
-        url = input("Please input a URL for a website to search, then hit ENTER.\n\t=> ") # get URL from user
+        url = input("Please input a full URL for a website to search, then hit ENTER.\n\t=> ") # get URL from user
         if not url:
             print("\nYou must enter a URL.\n")
         else:
@@ -35,21 +35,49 @@ def url_search(url):
     j
 
 def download():
-    r
-def mkdir():
-    r
+    
+    file_path = input("\nEnter file path for custom save location or leave blank to use your current directory. Example </home/Rico>")
+
+    if not file_path:
+        file_path = os.Path.cwd()
+    
+    folder = os.path.dirname(file_path)
+    
+    if not os.path.exists(folder):
+        mkdir(folder)
+
+def mkdir(folder):
+    mkdir = input(f"The directory {{folder}} does not exist.\nWould you like to create it? (y/n): ").strip().lower()
+
+    if mkdir == "y":
+        try:
+            os.makedirs(folder)
+            print(f"Created directory: {folder}")
+        except Exception as e:
+            print(f"Error: {e}")
+    else:
+        print("\nFiles not saved. Exiting program.")
+        time.sleep(1)
+        exit()
 
 ### Start of program ###
 print("=" * 60)
 print("Image downloader".center(60))
 print("=" * 60)
-startText = textwrap.dendent("""
+startText = textwrap.dedent("""
+        ***************
+        *** WARNING ***
+        ***************
         This is a program to search a image sharing site.
         It will then download those images to a desiered location.
-        ***WARNING***
         This may result in a large number of downloads.""")
 print(startText)
 start = input("\nPress 'Y' then hit ENTER to continue.\nOtherwise enter any key or leave blank and hit ENTER to exit.").strip().lower()
 if start == "y":
     print("\n---PROGRAM STARTING---\n")
+    time.sleep(1)
     user_input()
+else:
+    print("Goodbye!")
+    time.sleep(1)
+    exit()
