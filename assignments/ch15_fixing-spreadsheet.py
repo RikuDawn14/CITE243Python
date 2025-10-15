6176daea6ddc8d72259fe942d1679b582c39fb90
RikuDawn14 <180044602+RikuDawn14@users.noreply.github.com>
2025-10-15 09:13:21 -0700

    Create assignment and set goals

---
+++ assignments/ch15_fixing-spreadsheet.py
@@ -0,0 +1,21 @@
+# Find issue in speadsheet and fix
+
+# Google sheet to use "https://docs.google.com/spreadsheets/d/1jDZEdvSIh4TmZxccyy0ZXrH-ELlrwq8_YYiZrEOB4jg"
+
+"""
+===========================================================
+Description:
+    This program goes to a googlesheet searches for and issue
+    then allows user to fix that issue.
+Usage:
+    Run the script using Python 3.13.7. Ensure all dependencies
+    are installed before execution.
+===========================================================
+"""
+# Get URL for googlesheet from user
+# Verify URL is valid
+# look at BEANS PER JAR and JARS in a row add the numbers together and compare to TOTAL BEANS
+# If numbers match move to next row
+# If not, alert user of what row and offer to replace number
+# Get user input on fix then continue.
+# After done list number of rows checked, how many issues found, and how many changed
