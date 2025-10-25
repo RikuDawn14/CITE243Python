184f974c55e4f57f2ad4874c2ca5c9cfc6b5fb13
RikuDawn14 <180044602+RikuDawn14@users.noreply.github.com>
2025-10-24 17:42:10 -0700

    Create and add goals

---
+++ assignments/ch17_Dictionary-attack.py
@@ -0,0 +1,21 @@
+# Ch 17 PDF Password Breaker
+
+"""
+===========================================================
+Program Name: ch17 Dictionary Attack
+Author: Matthew Balthaser
+Date: 2025-10-26
+Description:
+    This program preforms a dictionary attack on a password protected 
+    PDF file.
+Usage:
+    Run the script using Python 3.13.7. Ensure all dependencies
+    are installed before execution.
+===========================================================
+"""
+
+# Get path to PDF
+# Get path to dictionary file
+# Make loop to try passwords on file (both upper and lower versions)
+# Stop loop when password found
+# Print password that worked
