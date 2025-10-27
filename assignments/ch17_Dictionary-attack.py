2f8a0bd77902a1ce19f959437ceadc7b5654a8ea
RikuDawn14 <180044602+RikuDawn14@users.noreply.github.com>
2025-10-27 08:23:13 -0700

    start

--- assignments/ch17_Dictionary-attack.py
+++ assignments/ch17_Dictionary-attack.py
@@ -20,5 +20,37 @@ Usage:
 # Stop loop when password found
 # Print password that worked
 
-with open(dic_path, 'r') as dictionary:
-    dic_list = dictionary.read().splitlines()
+import time
+import pypdf
+
+
+def user_in():
+    dic_path = input("Please enter the path to your dictionary file for attack then hit ENTER.\n\t=> ")
+    
+    with open(dic_path, 'r') as dictionary:
+        dic_list = dictionary.read().splitlines()
+
+
+
+
+
+
+
+
+### Start of program ###
+print("=" * 60)
+print("PDF Password Dictionary Attack".center(60))
+print("=" * 60)
+
+start = input("\nPress 'Y' then hit ENTER to continue.\nOtherwise enter any key or leave blank and hit ENTER to exit.\n\t=> ").strip().lower()
+if start == "y":
+    print("\n---PROGRAM STARTING---\n")
+    time.sleep(.5)
+
+
+
+else:
+    time.sleep(.5)
+
+print("\n---EXITING PROGRAM---\n")
+input("Press ENTER to exit.")
