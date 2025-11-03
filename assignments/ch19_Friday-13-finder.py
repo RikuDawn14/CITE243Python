3be88139c936e96347646bb07935bc270243e6ad
RikuDawn14 <180044602+RikuDawn14@users.noreply.github.com>
2025-11-03 09:16:12 -0800

    creation

---
+++ assignments/ch19_Friday-13-finder.py
@@ -0,0 +1,22 @@
+# ch 19 Friday the 13th Finder
+
+"""
+===========================================================
+Program Name: ch19 Friday the 13th Finder
+Author: Matthew Balthaser
+Date: 2025-11-09
+Description:
+    This program preforms a search and lists when firday the 13th are.
+Usage:
+    Run the script using Python 3.13.7. Ensure all dependencies
+    are installed before execution.
+===========================================================
+"""
+# Write two programs
+
+# Create a datetime object for the current day and a timedelta object of one day
+# If the current day is a Friday the 13th, it should print the month and year
+# Add the timedelta object to the datetime object to set it to the next day, and repeat the check
+# Repeat until it has found the next ten Friday the 13th dates
+
+# The second program should do the same thing except subtract the timedelta object. This program will find all of the past months and years with a Friday the 13th, and stop when it reaches the year 1
