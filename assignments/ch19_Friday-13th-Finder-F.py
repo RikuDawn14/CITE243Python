10b69241610469f24776557214c7c33ecf9e59e5
RikuDawn14 <180044602+RikuDawn14@users.noreply.github.com>
2025-11-04 11:25:13 -0800

    Create loop

--- assignments/ch19_Friday-13th-Finder-F.py
+++ assignments/ch19_Friday-13th-Finder-F.py
@@ -1,1 +1,17 @@
 #ch19_Friday-13th-Finder-F
+
+loop = 0
+cur_date = datetime.datetime.now()
+date = cur_date
+delta = datetime.timedelta(days=1)
+spooky = []
+while loop < 10:
+    if date.weekday() == 4 and date.day == 13:
+        pretty_date = date.strftime("%B %d, %Y")
+        spooky.append(pretty_date)
+        loop += 1
+    date = date + delta
+
+print("Here are the next ten (10) friday the 13ths:")
+for i in spooky:
+    print(i)
