34dce00beb2d618b1d00b0fe0ed9ff96d8ba1034
RikuDawn14 <180044602+RikuDawn14@users.noreply.github.com>
2025-11-04 13:04:37 -0800

    adjust time scale

--- assignments/ch19_Friday-13th-Finder-P.py
+++ assignments/ch19_Friday-13th-Finder-P.py
@@ -3,18 +3,16 @@
 import datetime
 
 def find_past_fridays():
-    loop = 0
     cur_date = datetime.datetime.now()
     date = cur_date
     delta = datetime.timedelta(days=1)
     spooky = []
-    while loop < 365:
+    while date.year > 0:
         if date.weekday() == 4 and date.day == 13:
             pretty_date = date.strftime("%B %d, %Y")
             spooky.append(pretty_date)
-        loop += 1
         date = date - delta
 
-    print("Here are the friday the 13ths in the last year:")
+    print("Here are the friday the 13ths in the past:")
     for i in spooky:
         print(i)
